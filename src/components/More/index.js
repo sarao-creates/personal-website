34d160e146ec5475e482261cb9eacc6d26bc3057
Sameer Rao <sarao@live.unc.edu>
2022-12-26 23:32:25 -0500

    bunch of changes

--- src/components/More/index.js
+++ src/components/More/index.js
@@ -6,7 +6,7 @@ function More() {
             <NavBar />
             <div class='container mx-auto w-3/4 pt-16 sm:w-full'>
                 <h1 class='font-mono font-extrabold text-6xl py-5 sm:text-center'>More!</h1>
-                <p class='font-mono text-base sm:text-center'>Photography. More photos <a target='_blank' rel='noopener noreferrer' href='https://vsco.co/sameerphotography/gallery'><u>here</u></a>. Instagram <a target='_blank' rel='noopener noreferrer' href="https://instagram.com/shotsbysameer"><u>here</u></a>. WIP website <a target='_blank' rel='noopener noreferrer' href="https://tender-rosalind-229394.netlify.app/"><u>here</u></a>.</p>
+                <p class='font-mono text-base sm:text-center'>Photography. More photos <a target='_blank' rel='noopener noreferrer' href='https://vsco.co/sameerphotography/gallery'><u>here</u></a>. Instagram <a target='_blank' rel='noopener noreferrer' href="https://instagram.com/shotsbysameer"><u>here</u></a>.</p>
                 <div class='flex space-x-4 pt-4 sm:block sm:p-0'>
                     <div class='w-1/3 h-96 bg-eiffel bg-cover sm:w-11/12 sm:mx-auto sm:rounded sm:mt-4'></div>
                     <div class='w-1/3 h-96 bg-basilica bg-cover sm:w-11/12 sm:mx-auto sm:rounded sm:mt-4'></div>
@@ -18,7 +18,7 @@ function More() {
                     <div class='w-1/3 h-96 bg-sacre bg-cover sm:w-11/12 sm:mx-auto sm:rounded sm:mt-4'></div>
                 </div>
 
-                <p class='font-mono text-base pt-8 sm:text-center'>Startups.</p>
+                <p class='font-mono text-base pt-8 sm:text-center'>Startups. WIP - trying to get more involved with startups and venture capital. So far have just been learning and reading newsletters but would love to learn more so please reach out!</p>
                 <p class='font-mono text-base pt-8 sm:text-center'>Music. Note: Spotify Widget Embeds only show the canonical order as of now. Visit my playlist to see the custom sorting order, which features recently added songs at the top.</p>
                 <div class='pt-4'>
                     <iframe src="https://open.spotify.com/embed/playlist/7epZVe62LRTtODsMFBVY4R?theme=0" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
--- src/components/Writing/index.js
+++ src/components/Writing/index.js
@@ -13,7 +13,7 @@ function Writing() {
             <div>
                 <div class='container mx-auto w-3/4 pt-16'>
                     <h1 class='font-mono font-extrabold text-6xl py-5'>Writing</h1>
-                    <p class='font-mono text-base'>About tech, products, consulting, startups, and human understanding.</p>
+                    <p class='font-mono text-base'>About tech, products, consulting, startups, and experiences. I'm somewhat active, trying to write more!</p>
                     <p class='font-mono text-base'><a target='_blank' rel='noopener noreferrer' href='https://sameer-rao.medium.com'><u>sameer-rao.medium.com</u></a></p>
                     {/* <div id="medium-widget"></div> */}
    
